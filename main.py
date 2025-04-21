59a9ef5a3cb1a3c7931c79cc3dbb5c53a93777d6
Jusumgi <umgijus@gmail.com>
2025-04-21 15:23:27 -0400

    load on main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cashflow
 import getch
 import os
+import getfiles
+import copy
 
 folder_path = "saves/"
 
@@ -8,20 +10,31 @@ if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
 def main():
+    print("Welcome to Expense Tracker")
+    print("(S)tart Fresh or (L)oad?")
+    choice = getch.getch()
+    if choice == "s":
+        pass
+    elif choice == "l":
+        files = getfiles.get_file_names("saves/")
+        for each in files:
+            print(each)
+        file = input("Enter file name: ")
+        loaded_file = cashflow.load_cashflow(file)
     while True:
         print("Expense Tracker Main Menu")
-        print("(1) View Plan")
+        print("(1) Show Loaded File (Temporary)")
         print("(2) View Statistics")
         print("(3) Edit Cashflow")
         print("(q) Exit")
         choice = getch.getch()
         match(choice):
             case "1":
-                pass
+                print(loaded_file)
             case "2":
                 pass
             case "3":
-                cashflow.cashflowed()
+                loaded_file = cashflow.cashflowed()
             case "q":
                 break
 main()
--- people.py
+++
@@ -1,7 +0,0 @@
-people = []
-def add_people(people):
-    person_add = input("Enter a name to be added: ")
-    people.append(person_add)
-def remove_people(people):
-    person_remove = input("Enter a name to be removed: ")
-    people.pop(person_remove)
